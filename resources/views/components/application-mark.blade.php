9bfc81edd8d91b99c9e3e1f0531779e19cc5ed1b
RocioMarco1011 <rociomarco1011@gmail.com>
2023-10-12 22:53:44 -0600

    main

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,5 @@
 <a href="/dashboard">
-  <span style="font-size: 24px;">Secretaria de las Mujeres</span>
+  <span style="font-size: 24px;">
+    <img src="{{ asset('images/logo2.jpg') }}" alt="Logo" style="width: 164px; height: auto;">
+  </span>
 </a>
