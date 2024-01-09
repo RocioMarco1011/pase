b28e9c90cc7f2ee9a174b370c9e61a76cdfa58dc
rociomarco1011 <rociomarco1011@gmail.com>
2024-01-08 22:34:20 -0600

    PARTE FINAL 2/3

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -5,6 +5,22 @@
     <!-- Estilos CSS aquí si es necesario -->
 </head>
 <body>
+    <h1>INFORMACIÓN DEL INDICADOR</h1>
+
+    <p><strong>Nombre:</strong> {{ $indicadorprevenir->nombre }}</p>
+    <p><strong>Objetivo:</strong> {{ $indicadorprevenir->objetivo }}</p>
+    <p><strong>Definición:</strong> {{ $indicadorprevenir->definicion }}</p>
+    <p><strong>Variables:</strong> {{ $indicadorprevenir->variables }}</p>
+    <p><strong>Observaciones:</strong> {{ $indicadorprevenir->observaciones }}</p>
+    <p><strong>Medios de Verificación:</strong> {{ $indicadorprevenir->medios_verificacion }}</p>
+    <p><strong>Parámetro/Meta:</strong> {{ $indicadorprevenir->parametro_meta }}</p>
+    <p><strong>Unidad de Medida:</strong> {{ $indicadorprevenir->unidad_medida }}</p>
+    <p><strong>Nivel de Desagregación:</strong> {{ $indicadorprevenir->nivel_desagregacion }}</p>
+    <p><strong>Acumulado/Periodico:</strong> {{ $indicadorprevenir->acumulado_periodico }}</p>
+    <p><strong>Tendencia Esperada:</strong> {{ $indicadorprevenir->tendencia_esperada }}</p>
+    <p><strong>Frecuencia de Medición:</strong> {{ $indicadorprevenir->frecuencia_medicion }}</p>
+    <p><strong>Semaforo:</strong> {{ $indicadorprevenir->semaforo }}</p>
+
     <h1>RESULTADOS DE CALCULO</h1>
 
     @if($calculo->count() > 0)
